427332733999d9629787212db107ce7518f11c19
hendisantika <hendisantika@yahoo.co.id>
2025-11-13 22:21:50 +0700

    feat: Init Project 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/multipostgres/SpringBootMultiPostgresApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.multipostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiPostgresApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiPostgresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/multipostgres/SpringBootMultiPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.multipostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
