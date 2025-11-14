f005227b258184b8a0acae2b98299f47e3143009
hendisantika <hendisantika@yahoo.co.id>
2025-11-14 07:01:18 +0700

    feat: Add required libs 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/multipostgres/SpringBootMultiPostgresApplication.java
+++ src/main/java/id/my/hendisantika/multipostgres/SpringBootMultiPostgresApplication.java
@@ -2,8 +2,9 @@ package id.my.hendisantika.multipostgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringBootMultiPostgresApplication {
 
     static void main(String[] args) {
